ea9dcb50c2a0771cc8d9c870851beff241e02df9
Hannah Park <teyang610@gmail.com>
2017-07-06 12:52:58 -0800

    class

---
+++ IntentExample/app/src/main/java/com/example/ciccc_cirac/intentexample/IntentexampleB.java
@@ -0,0 +1,16 @@
+package com.example.ciccc_cirac.intentexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by CICCC-CIRAC on 2017-07-06.
+ */
+
+public class IntentexampleB extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intentexample2);
+    }
+}
