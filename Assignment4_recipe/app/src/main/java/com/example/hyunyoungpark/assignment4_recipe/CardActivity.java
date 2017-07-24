b9126a5e5fe8dd911a4f7d0bb34360d2f1831f5b
Hannah Park <teyang610@gmail.com>
2017-07-24 14:57:58 -0700

    assignment4

---
+++ Assignment4_recipe/app/src/main/java/com/example/hyunyoungpark/assignment4_recipe/CardActivity.java
@@ -0,0 +1,53 @@
+package com.example.hyunyoungpark.assignment4_recipe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by hyunyoungpark on 2017-07-24.
+ */
+
+public class CardActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+
+        public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ Assignment4_recipe/app/src/main/java/com/example/hyunyoungpark/assignment4_recipe/RecyclerAdapter.java
@@ -0,0 +1,97 @@
+package com.example.hyunyoungpark.assignment4_recipe;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by hyunyoungpark on 2017-07-24.
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private String[] titles = {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"
+
+    };
+
+    private String[] details = {
+            "1 details",
+            "2 details",
+            "3 details",
+            "4 details",
+            "5 details",
+            "6 details",
+            "7 details",
+            "8 details"
+    };
+
+    private int[] images = {
+            R.drawable.tteokbokki,
+            R.drawable.bulgogi,
+            R.drawable.chickenbbq,
+            R.drawable.japchae,
+            R.drawable.jeon,
+            R.drawable.kimchijjigae,
+            R.drawable.jjimdak,
+            R.drawable.samgyeopsal
+    };
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public int currentItem;
+        public ImageView itemImage;
+        public TextView itemTitle;
+        public TextView itemDetail;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemImage = (ImageView)itemView.findViewById(R.id.item_image);
+            itemTitle = (TextView)itemView.findViewById(R.id.item_title);
+            itemDetail = (TextView)itemView.findViewById(R.id.item_detail);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Snackbar.make(v, "Click detected on item " + position,
+                            Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_layout, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        viewHolder.itemTitle.setText(titles[i]);
+        viewHolder.itemDetail.setText(details[i]);
+        viewHolder.itemImage.setImageResource(images[i]);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return titles.length;
+    }
+}
