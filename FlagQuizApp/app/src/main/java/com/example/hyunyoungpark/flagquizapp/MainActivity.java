23fdb91e331aba2e8763f71effdef092c75ce804
Hannah Park <teyang610@gmail.com>
2017-08-04 13:31:04 -0700

    Flag quiz app

--- FlagQuizApp/app/src/main/java/com/example/hyunyoungpark/flagquizapp/MainActivity.java
+++ FlagQuizApp/app/src/main/java/com/example/hyunyoungpark/flagquizapp/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.hyunyoungpark.flagquizapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String CHOICES = "pref_numberOfChoices";
     public static final String REGIONS = "pref_regionsToInclude";
+
+    private boolean phoneDevice = true;
+    private boolean preferencesChanged = true;
 
 
     @Override
@@ -24,7 +35,59 @@ public class MainActivity extends AppCompatActivity {
 
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(preferenceChangeListener);
+
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE || screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE) phoneDevice = false;
+
+        if (phoneDevice) setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(preferencesChangeListener);
     }
+
+    private OnSharedPreferenceChangeListener preferenceChangeListener = new OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(
+                SharedPreferences sharedPreferences, String key) {
+            preferencesChanged = true;
+
+            MainActivityFragment quizFragment = (MainActivityFragment)
+                    getSupportFragmentManager().findFragmentById(
+                            R.id.quizFragment);
+
+            if (key.equals(CHOICES)) {
+                quizFragment.updateGuessRows(sharedPreferences);
+                quizFragment.startQuiz();
+            }
+            else if (key.equals(REGIONS)) {
+                Set<String> regions =
+                        sharedPreferences.getStringSet(REGIONS, null);
+
+                if (regions != null && regions.size() > 0) {
+                    quizFragment.updateRegions(sharedPreferences);
+                    quizFragment.startQuiz();
+                }
+                else {
+                    SharedPreferences.Editor editor =
+                            sharedPreferences.edit();
+                    regions.add(getString(R.string.default_region));
+                    editor.putStringSet(REGIONS, regions);
+                    editor.apply();
+
+                    Toast.makeText(MainActivity.this,
+                            R.string.default_region_message,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            Toast.makeText(MainActivity.this,
+                    R.string.restarting_quiz,
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
 
     @Override
     protected void onStart() {
@@ -34,12 +97,20 @@ public class MainActivity extends AppCompatActivity {
         quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
         quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
         quizFragment.startQuiz();
+        preferencesChanged = false;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
+        else
+            return false;
     }
 
     @Override
